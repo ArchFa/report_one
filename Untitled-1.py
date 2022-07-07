32d152be1ce9e365a59dbc2c6e6f9a67b0d23355
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-07-07 20:08:11 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,8 @@
 # %%
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+
 
 # %%
 count_task = "offers_statuses (3).txt"
@@ -18,7 +20,6 @@ df.offer_created_at = df.offer_created_at.values.astype('M8[D]')
 
 df['count_responds'] = df['count_responds'].astype(int)
 df['count_prematch'] = df['count_prematch'].astype(int)
-
 
 df['platform'] = df['platform'].str.strip()
 
@@ -58,11 +59,21 @@ create_task_mobile_june = task_mobile_june * 100 / all_task_may
 
 # %%
 st.title("Отчет UrgY")
-
-# %%
 st.subheader("Количество созданных задач")
 
 # %%
 st.metric(label="Количество созданных задач за июнь", value=all_task_june, delta=difference_task)
+
+# %%
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
 
 
