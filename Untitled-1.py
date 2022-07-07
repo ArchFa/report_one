606cfa611eb42603c8277db55917f3ce3e14cef3
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-07-07 20:43:54 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,10 +3,11 @@ import pandas as pd
 import streamlit as st
 
 # %%
-count_task = "offers_statuses (3).txt"
+#count_task = "offers_statuses (3).txt"
 
 # %%
-df = pd.read_csv(count_task, sep='|')
+#df = pd.read_csv(count_task, sep='|')
+df = pd.read_csv('/Users/arturfattahov/Downloads/Telegram Desktop/offers_statuses (3).txt', sep='|')
 
 # %%
 df = df.dropna()
@@ -40,19 +41,18 @@ create_task_mobile_may = task_mobile_may * 100 / all_task_may
 create_task_mobile_june = task_mobile_june * 100 / all_task_may
 
 # %%
+task_admins_j = df_june['platform'].value_counts()[0]
+task_andr_j = df_june['platform'].value_counts()[1]
+task_ios_j = df_june['platform'].value_counts()[2]
+
+task_admins_m = df_may['platform'].value_counts()[0]
+task_andr_m = df_may['platform'].value_counts()[1]
+task_ios_m = df_may['platform'].value_counts()[2]
 
 
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
+diff_1 = task_admins_j - task_admins_m
+diff_2 = task_andr_j - task_andr_m
+diff_3 = task_ios_j - task_ios_m
 
 # %%
 st.title("Отчет UrgY")
@@ -62,27 +62,10 @@ st.subheader("Количество созданных задач")
 st.metric(label="Количество созданных задач за июнь", value=all_task_june, delta=difference_task)
 
 # %%
-import plotly.figure_factory as ff
-import numpy as np
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
-
-# %%
-
+col1, col2, col3 = st.columns(5)
+col1.metric("Количество созданных задач за июнь", all_task_june, difference_task)
+col2.metric("Количество созданных задач за июнь Android", task_andr_j, diff_2)
+col3.metric("Количество созданных задач за июнь iOS", task_ios_j, diff_3)
+col3.metric("Количество созданных задач за июнь Admins", task_admins_j,diff_1)
 
 
